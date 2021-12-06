caa397f1cd7e1bfcd128dad306e2c9793fa131a0
Bruno Bonnin <brunoebonnin@gmail.com>
2021-12-06 17:52:51 -0300

    Agregado control de fecha de nacimiento

--- CentroDeVacunacion/ConsolaUI/PersonaIO.cs
+++ CentroDeVacunacion/ConsolaUI/PersonaIO.cs
@@ -1,6 +1,7 @@
 ﻿using CentroDeVacunacion.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,13 +29,35 @@ namespace CentroDeVacunacion.ConsolaUI
             dni = IngresarDNI();
             Console.WriteLine("Ingrese Pais de Origen");
             nacionalidad = Console.ReadLine();
-            Console.WriteLine("Ingrese la fecha de Nacimiento en el Formato dd/MM/yyyy");
-            fechaDeNacimiento = DateTime.Parse(Console.ReadLine());
+            fechaDeNacimiento = IngresoFechaNacimiento();
             Direccion direccion = this.direccionIO.IngresarDireccion();
             Informacion informacion = this.informacionIO.IngresarInformacion();
 
 
             return new Persona(nombre, apellido, claseDni, dni, nacionalidad, fechaDeNacimiento, direccion, informacion);
+        }
+
+        public DateTime IngresoFechaNacimiento()
+        {
+            CultureInfo cultura = CultureInfo.CreateSpecificCulture("es-AR");
+            DateTime fechaDeNacimiento = DateTime.Now;
+            bool aux = false;
+
+            while (!aux)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese la fecha de Nacimiento en el Formato dd/MM/yyyy");
+                    DateTime newFechaDeNacimiento = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy", cultura);
+                    fechaDeNacimiento = newFechaDeNacimiento;
+                    aux = true;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Debe ingresar la Fecha de Nacimiento en el formato dd/MM/yyyy");
+                }
+            }
+            return fechaDeNacimiento;
         }
 
         public int IngresarDNI()
